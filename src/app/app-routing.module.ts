395269210fddba14d5fe1988f3f37922398df3d8
Dushyantha Kalehewatte <dushyanthak@outlook.com>
2023-06-12 18:55:07 +0530

    current Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AddcontactComponent } from './addcontact/addcontact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "Home", component: HomeComponent },
+  { path: "About", component: AboutComponent },
+  {
+    path: "Contact",
+    component: ContactComponent,
+    children: [
+      { path:"Add" , component:AddcontactComponent} , 
+      { path:"Edit/:id" , component:AddcontactComponent}
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
